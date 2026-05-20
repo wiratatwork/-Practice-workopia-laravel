a7bc93770e1203aa9bd01327156c006526088513
Wirat Sakorn <wiratatwork@gmail.com>
2026-05-20 11:09:25 +0700

    route-parameters

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/test-link', function () {
     $url = route('jobs.index');
     return "<a href='$url'>Click here to see all jobs</a>";
 });
+
+Route::get('/listings/{id}', function (string $id) {
+    return 'Listing ID: ' . $id;
+})->whereNumber('id');
+
+Route::get('/category/{category}/post/{post_id}', function (string $category, string $post_id) {
+    return 'Category: ' . $category . ' | Post ID: ' . $post_id;
+});
