e9fbc56e0b074d95d67e66c8009f1024cf20c8c0
Wirat Sakorn <wiratatwork@gmail.com>
2026-05-20 13:15:25 +0700

    request-object

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,4 +26,20 @@ Route::get('/listings/{id}', function (string $id) {
 
 Route::get('/category/{category}/post/{post_id}', function (string $category, string $post_id) {
     return 'Category: ' . $category . ' | Post ID: ' . $post_id;
+});
+
+Route::get('/user-info', function (Request $request) {
+    return [
+        'ip_address' => $request->ip(),
+        'browser_info' => $request->userAgent()
+    ];
+});
+
+Route::get('/search', function (Request $request) {
+    return $request->only(['keyword', 'location']);
+});
+
+Route::get('/check-user', function (Request $request) {
+    $name = $request->input('name', 'Guest');
+    return "Welcome, " . $name;
 });
