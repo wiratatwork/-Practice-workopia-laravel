fa044a10f52e3037c0f68af065dc8c1542c3c5ca
Wirat Sakorn <wiratatwork@gmail.com>
2026-05-20 10:57:40 +0700

    Implement basic and named routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return '<h1>Contact Us</h1>';
+});
+
+Route::get('/all-jobs', function () {
+    return 'Available Jobs';
+})->name('jobs.index');
+
+Route::get('/test-link', function () {
+    $url = route('jobs.index');
+    return "<a href='$url'>Click here to see all jobs</a>";
+});
